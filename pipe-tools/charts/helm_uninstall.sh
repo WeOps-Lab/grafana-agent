3934de4b995b3d265f0ef85ae4a47f5abe5faa05
zhongming Fan <zhongmingfan@canway.net>
2023-02-21 15:13:19 +0800

    feature: grafana agent

--- pipe-tools/charts/helm_uninstall.sh
+++ pipe-tools/charts/helm_uninstall.sh
@@ -4,8 +4,10 @@
 NAMESPACE="redis"
 
 # Uninstall Redis deployments
-for RELEASE in $(helm list -n $NAMESPACE --short)
-do
-  echo "Uninstalling $RELEASE ..."
-  helm uninstall -n $NAMESPACE $RELEASE
-done
+#for RELEASE in $(helm list -n $NAMESPACE --short)
+#do
+#  echo "Uninstalling $RELEASE ..."
+#  helm uninstall -n $NAMESPACE $RELEASE
+#done
+
+helm ls --namespace $NAMESPACE | grep redis | awk '{print $1}' | xargs -I {} helm delete --purge {}
